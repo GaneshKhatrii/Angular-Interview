d0d913a37be14c7d691b80078b0dc7f83bd4f6d0
Ganesh Basude <basudeganesh7@gmail.com>
2026-09-03 16:08:52 +0530

    change

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,19 +1,18 @@
-import { Component, signal } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIcon } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-
+import { Component, signal } from "@angular/core";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIcon } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
 @Component({
-  selector: 'app-login',
-  imports: [MatFormFieldModule, MatInputModule, MatIcon],
-  templateUrl: './login.html',
-  styleUrl: './login.scss',
+  selector: "app-login",
+  imports: [MatFormFieldModule, MatInputModule, MatIcon, MatButtonModule],
+  templateUrl: "./login.html",
+  styleUrl: "./login.scss",
 })
 export class Login {
   hidePassword = signal<boolean>(true);
 
-  togglePasswordVisibility(event: MouseEvent): void {
+  togglePasswordVisibility(): void {
     this.hidePassword.update((value) => !value);
-    event.stopPropagation();
   }
 }
